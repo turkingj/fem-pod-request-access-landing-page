f049a508c927c58953fcded58d5fbe07a9eddeaf
Jonathan Turkington <jonathan@jturkington.com>
2020-12-02 12:13:12 +0000

    feat(*): progress to date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pod-request-access-landing-page';
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  /**
+   * getters
+   */
+  get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+
+  /**
+   * form errors
+   */
+  emailControlInvalid(): string {
+    return this.email.touched && this.email.hasError('required')
+      ? 'Oops! Please check your email'
+      : this.email.touched && this.email.hasError('email')
+      ? 'Oops! Please check your email'
+      : '';
+  }
+
+  requestAccess(): void {
+    const { value, valid } = this.form;
+    if (valid) {
+      console.log('TODO send email!');
+      // this.sendEmail.emit(value);
+    }
+  }
 }
